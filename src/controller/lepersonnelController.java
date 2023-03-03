b2d4c1232d2d8098b1f9c1c5e6900ae6945814e8
Jordan TATUE <122097859+JordanTatue@users.noreply.github.com>
2023-03-03 11:11:53 +0100

    first git

---
+++ src/controller/lepersonnelController.java
@@ -0,0 +1,82 @@
+package controller;
+
+import com.jfoenix.controls.JFXComboBox;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class lepersonnelController {
+
+    @FXML
+    private TableColumn<?, ?> cmailp;
+
+    @FXML
+    private TableColumn<?, ?> cnomp;
+
+    @FXML
+    private TableColumn<?, ?> ctelp;
+
+    @FXML
+    private TableColumn<?, ?> ctypep;
+
+    @FXML
+    private ImageView imp;
+
+    @FXML
+    private AnchorPane p;
+
+    @FXML
+    private TextField tfidp;
+
+    @FXML
+    private TextField tfmailp;
+
+    @FXML
+    private PasswordField tfmdpp;
+
+    @FXML
+    private TextField tfnomp;
+
+    @FXML
+    private TextField tfphotop;
+
+    @FXML
+    private TextField tftelp;
+
+    @FXML
+    private JFXComboBox<?> tftypep;
+
+    @FXML
+    private TableView<?> tvpersonnel;
+
+    @FXML
+    void ajouter_personnel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void inserer(ActionEvent event) {
+
+    }
+
+    @FXML
+    void modifier_personnel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void rechercher(ActionEvent event) {
+
+    }
+
+    @FXML
+    void supprimer_personnel(ActionEvent event) {
+
+    }
+
+}
